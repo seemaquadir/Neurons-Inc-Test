b8fe765656baf06f09a9928a82da66105723a81b
seemaquadir <87022202+seemaquadir@users.noreply.github.com>
2021-07-07 17:27:11 +0200

    Update index.js

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -1,23 +1,10 @@
-import React from "react";
-import "./styles.css";
-import getRandomFruitsName from "random-fruits-name";
-
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Random Fruits name with React example</h1>
-      <h2>This npm package of generate the names of random fruits.</h2>
-      <p>{getRandomFruitsName() /* English is default language */}</p>
-      <p>{getRandomFruitsName("en")}</p>
-      <p>{getRandomFruitsName("en")}</p>
-      <p>{getRandomFruitsName("en")}</p>
-      <p>{getRandomFruitsName("en")}</p>
-      <p>{getRandomFruitsName("en")}</p>
-      <p>{getRandomFruitsName("en")}</p>
-      <p>{getRandomFruitsName("en")}</p>
-      <p>{getRandomFruitsName("en")}</p>
-      <p>{getRandomFruitsName("en")}</p>
-      <p>{getRandomFruitsName("en")}</p>
-    </div>
-  );
-}
+const getRandomFruitsName = require('random-fruits-name')
+console.log(getRandomFruitsName()) // Apple - English is default language
+console.log(getRandomFruitsName('en')) 
+console.log(getRandomFruitsName('en')) 
+console.log(getRandomFruitsName('en')) 
+console.log(getRandomFruitsName('en')) 
+console.log(getRandomFruitsName('en')) 
+console.log(getRandomFruitsName('en')) 
+console.log(getRandomFruitsName('en')) 
+console.log(getRandomFruitsName('en')) 
