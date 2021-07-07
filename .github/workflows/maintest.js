428c31e3cbe2f00e3aca2848ae4a6aab55902c2c
seemaquadir <87022202+seemaquadir@users.noreply.github.com>
2021-07-07 20:07:50 +0200

    Update maintest.js

--- .github/workflows/maintest.js
+++ .github/workflows/maintest.js
@@ -26,39 +26,7 @@ describe('random-fruits-name', () => {
       })
     })
 
-    describe('If `maxWords: 2` is specified.', () => {
-      test('Select English', () => {
-        const fruitsName = getRandomFruitsName('en', { maxWords: 2 })
-        expect(fruitsName).toBeTruthy()
-
-        const wordCount = fruitsName.split(' ').length
-        expect(wordCount <= 2).toBeTruthy()
-      })
-
-     
-      })
-  
-
-    describe('If `maxWords` is not a number (strings case).', () => {
-      test('Select English', () => {
-        expect(() => getRandomFruitsName('en', { maxWords: '1' })).toThrow()
-      })
-    })
-
-    describe('If `maxWords` is not a number (float case).', () => {
-      test('Select English', () => {
-        expect(() => getRandomFruitsName('en', { maxWords: 1.23 })).toThrow()
-      })
-    })
-
-    describe('If `maxWords` is not a number (boolean case).', () => {
-      test('Select English', () => {
-        expect(() => getRandomFruitsName('en', { maxWords: true })).toThrow()
-        expect(() => getRandomFruitsName('en', { maxWords: false })).toThrow()
-      })
-
-    })
-
+   
     describe('If `maxWords` is not a number (value is empty case).', () => {
       test('Select English', () => {
         expect(() => getRandomFruitsName('en', { maxWords: '' })).toThrow()
@@ -67,12 +35,8 @@ describe('random-fruits-name', () => {
      
     })
 
-    describe('When `maxWords` is specified without `lang`.', () => {
-      test('Only specify `maxWords` as an option.', () => {
-        expect(() => getRandomFruitsName({ maxWords: 1 })).toThrow()
-      })
-    })
-  })
+ 
+
 
   describe('If specify a non-existent option', () => {
     test('Specify `test` option', () => {
